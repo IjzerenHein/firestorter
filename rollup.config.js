f7699250f37b8ab2ca8f7a5cf0791410240683ba
IjzerenHein <hrutjes@gmail.com>
2017-12-05 21:28:29 +0100

    Changed bundler to Rollup, in order to make it work with StackBlitz

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
+import progress from 'rollup-plugin-progress';
+
+export default {
+	entry: 'src/index.js',
+	dest: 'dist/firestorter.js',
+	format: 'umd',
+	// sourceMap: true,
+	moduleName: 'firestorter',
+	external: ['mobx'],
+	globals: {
+		mobx: 'mobx'
+	},
+	plugins: [
+		babel({
+			babelrc: false,
+			plugins: [
+				'transform-object-rest-spread',
+				'transform-class-properties',
+				'transform-flow-strip-types'
+			],
+			presets: ['es2015-rollup'],
+			exclude: 'node_modules/**'
+		}),
+		resolve({
+			jsnext: true,
+			main: true
+		}),
+		commonjs(),
+		filesize(),
+		progress()
+	]
+};
