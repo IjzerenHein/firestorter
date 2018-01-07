bd34189db29b1ea15b04974d9b7ce2cf90aeede4
IjzerenHein <hrutjes@gmail.com>
2018-01-07 19:56:37 +0100

    Added document snapshot tests

---
+++ test/Document.snapshot.test.js
@@ -0,0 +1,30 @@
+import { Document } from './init';
+
+test('no values', () => {
+	const doc = new Document();
+	expect(doc.snapshot).toBeUndefined();
+});
+
+test('valid snapshot', async () => {
+	expect.assertions(1);
+	const doc = new Document('artists/FooFighters');
+	await doc.fetch();
+	expect(doc.snapshot).toBeDefined();
+});
+
+test('changed snapshot', async () => {
+	expect.assertions(2);
+	const doc = new Document('artists/FooFighters');
+	await doc.fetch();
+	const snapshot = doc.snapshot;
+	expect(snapshot).toBeDefined();
+	await doc.fetch();
+	expect(doc.snapshot).not.toBe(snapshot);
+});
+
+/* test('initial snapshot', async () => {
+	expect.assertions(1);
+	const doc = new Document('artists/FooFighters');
+	await doc.fetch();
+	expect(doc.snapshot).toBeDefined();
+});*/
