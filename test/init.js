9e9109cba907feede3347eba50dd9b2244fe6a79
IjzerenHein <hrutjes@gmail.com>
2018-11-09 14:22:20 +0100

    Updated dependencies

--- test/init.js
+++ test/init.js
@@ -1,22 +1,34 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
-import { initFirestorter, getFirestore, getFirebase, getFirebaseApp, Collection, Document } from '../src';
-import { autorun, reaction, observable } from 'mobx';
+import {
+	initFirestorter,
+	getFirestore,
+	getFirebase,
+	getFirebaseApp,
+	Collection,
+	Document
+} from '../src';
+import { autorun, reaction, observable, configure } from 'mobx';
 import firebaseConfig from './firebaseConfig.json';
 
 let firebaseApp;
 
 beforeAll(() => {
+	// Initialize firebase
 	firebaseApp = firebase.initializeApp(firebaseConfig);
 	const firestore = firebase.firestore();
 	firestore.settings({ timestampsInSnapshots: true });
 
+	// Configure mobx strict-mode
+	// configure({ enforceActions: 'always' });
+
+	// Initialize firestorter
 	initFirestorter({ firebase: firebase, app: firebaseApp });
 
 	// Verify that firestorter is initialized correctly
-	if(!getFirebase()) throw new Error('getFirebase');
-	if(!getFirebaseApp()) throw new Error('getFirebaseApp');
-	if(!getFirestore()) throw new Error('getFirestore');
+	if (!getFirebase()) throw new Error('getFirebase');
+	if (!getFirebaseApp()) throw new Error('getFirebaseApp');
+	if (!getFirestore()) throw new Error('getFirestore');
 });
 
 afterAll(() => {
