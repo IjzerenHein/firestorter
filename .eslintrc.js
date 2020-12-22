807c0e930061414912ccebed6bf094cf69a06c64
Hein Rutjes <hrutjes@gmail.com>
2020-12-22 20:52:07 +0100

    update linter

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	root: true,
+	extends: [
+		'universe/native',
+		'universe/node',
+		'universe/web',
+		'universe/shared/typescript-analysis',
+	],
+	settings: {
+		react: {
+			version: '16.8',
+		},
+	},
+	rules: {
+		'spaced-comment': ['warn', 'always', { block: { balanced: true } }],
+	},
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx', '*.d.ts'],
+			parserOptions: {
+				project: './tsconfig.json',
+			},
+			rules: {
+				'@typescript-eslint/naming-convention': [
+					'warn',
+					{ selector: 'typeLike', format: ['PascalCase'] },
+					{ selector: 'enumMember', format: ['UPPER_CASE'] },
+				],
+				'no-unused-expressions': 'off',
+				'@typescript-eslint/no-unused-expressions': 'warn',
+			},
+		},
+	],
+};
