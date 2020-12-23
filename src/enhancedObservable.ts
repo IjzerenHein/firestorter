77f20d5bd8b6c98f1c1a9ac54ef9cc89cb695242
Hein Rutjes <hrutjes@gmail.com>
2020-12-23 09:43:44 +0100

    fix observations

--- src/enhancedObservable.ts
+++ src/enhancedObservable.ts
@@ -1,4 +1,4 @@
-import { _isComputingDerivation, getAtom, observable } from 'mobx';
+import { observable, onBecomeObserved, onBecomeUnobserved } from 'mobx';
 
 import { IEnhancedObservableDelegate } from './Types';
 
@@ -6,38 +6,24 @@ import { IEnhancedObservableDelegate } from './Types';
  * @ignore
  * Creates an observable which calls addObserverRef &
  * releaseObserverRef methods on the passed-in delegate class.
- * Effecitively, this allows Firestorter to track whether
+ * Effectively, this allows Firestorter to track whether
  * a Collection/Document is observed and real-time updating
  * needs to be enabled on it.
  */
 export function enhancedObservable(data: any, delegate: IEnhancedObservableDelegate): any {
   const o = Array.isArray(data) ? observable.array(data) : observable.box(data);
-
-  // Hook into the MobX observable and track
-  // Whether any Component is observing this observable.
-  const atom: any = getAtom(o);
-  const onBecomeUnobserved = atom.onBecomeUnobserved;
-  const reportObserved = atom.reportObserved;
   let isObserved = false;
-  atom.isPendingUnobservation = false;
-  atom.onBecomeUnobserved = () => {
-    // eslint-disable-next-line prefer-rest-params
-    const res = onBecomeUnobserved.apply(atom, arguments);
+  onBecomeUnobserved(o, undefined, () => {
     if (isObserved) {
       isObserved = false;
       delegate.releaseObserverRef();
     }
-    return res;
-  };
-  atom.reportObserved = () => {
-    // eslint-disable-next-line prefer-rest-params
-    const res = reportObserved.apply(atom, arguments);
-    if (!isObserved && _isComputingDerivation()) {
+  });
+  onBecomeObserved(o, undefined, () => {
+    if (!isObserved) {
       isObserved = true;
       delegate.addObserverRef();
     }
-    return res;
-  };
-
+  });
   return o;
 }
